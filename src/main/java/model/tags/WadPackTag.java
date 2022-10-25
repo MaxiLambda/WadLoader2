c0e505cd5655a8bfcf1f1bc6e4f2b0df1e790340
Maxi <maximilian.lincks@gmx.net>
2022-10-25 11:21:39 +0200

    added Annotations for Hibernate

--- src/main/java/model/tags/WadPackTag.java
+++ src/main/java/model/tags/WadPackTag.java
@@ -3,15 +3,22 @@ package model.tags;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 
 /**
  * Tag which groups Elements of a Wad-Pack
 */
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "WadPackTags")
 public class WadPackTag implements Tag{
 
     private static final TagType tagType = TagType.WAD_PACK_TAG;
+    @Column
     private String name;
 
     @Override
