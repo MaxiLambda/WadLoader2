3b96d3925735b904184398cdf8c60c7eb1b39b40
Maxi <maximilian.lincks@gmx.net>
2022-10-27 14:24:00 +0200

    added Function to obtain File extension

--- src/main/java/lincks/maximilian/wadloader2/utils/PathUtil.java
+++ src/main/java/lincks/maximilian/wadloader2/utils/PathUtil.java
@@ -5,6 +5,15 @@ import java.nio.file.Path;
 public class PathUtil {
     public static String fileNameWithoutExtension(Path path){
         String fileName = path.getFileName().toString();
-        return fileName.substring(0,fileName.lastIndexOf('.'));
+        //-1 if '.' is not part of the fileName
+        int lastIndex = fileName.lastIndexOf('.');
+        return lastIndex < 0 ? fileName : fileName.substring(0,lastIndex);
+    }
+
+    public static String getExtension(Path path){
+        String fileName = path.getFileName().toString();
+        //-1 if '.' is not part of the fileName
+        int lastIndex = fileName.lastIndexOf('.');
+        return lastIndex < 0 ? "" : fileName.substring(lastIndex);
     }
 }
