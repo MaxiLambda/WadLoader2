889038e59a403746528312d640fd16d1967e591d
Maxi <maximilian.lincks@gmx.net>
2022-10-30 18:59:18 +0100

    Refactored Application to use UIBase as swing component instead of beeing one itself

--- src/main/java/lincks/maximilian/wadloader2/WadLoader2Application.java
+++ src/main/java/lincks/maximilian/wadloader2/WadLoader2Application.java
@@ -4,63 +4,39 @@ import lincks.maximilian.wadloader2.domain.WadFileFinder;
 import lincks.maximilian.wadloader2.model.tags.WadTag;
 import lincks.maximilian.wadloader2.model.wads.Wad;
 import lincks.maximilian.wadloader2.repos.services.WadService;
+import lincks.maximilian.wadloader2.ui.UIBase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class WadLoader2Application extends JFrame{
+public class WadLoader2Application{
 
 	//TODO: build fitting ui
-	private void initUI() {
 
-		var quitButton = new JButton("Quit");
-
-		quitButton.addActionListener((ActionEvent event) -> {
-			System.exit(0);
-		});
-
-		createLayout(quitButton);
-
-		setTitle("Quit button");
-		setSize(300, 200);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
-	private void createLayout(JComponent... arg) {
-
-		var pane = getContentPane();
-		var gl = new GroupLayout(pane);
-		pane.setLayout(gl);
-
-		gl.setAutoCreateContainerGaps(true);
-
-		gl.setHorizontalGroup(gl.createSequentialGroup()
-				.addComponent(arg[0])
-		);
-
-		gl.setVerticalGroup(gl.createSequentialGroup()
-				.addComponent(arg[0])
-		);
-	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(WadLoader2Application.class)
-				.headless(false).run(args);
+				.web(WebApplicationType.NONE)
+				.headless(false)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
+
+		UIBase ui = new UIBase();
+
 
 		EventQueue.invokeLater(() -> {
-			WadLoader2Application ex = ctx.getBean(WadLoader2Application.class);
-			ex.setVisible(true);
+			ui.initUI();
+			ui.setVisible(true);
 		});
 	}
 
@@ -69,7 +45,6 @@ public class WadLoader2Application extends JFrame{
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void appStartup(){
-		initUI();
 		wadFinder.findWads(Path.of("D:\\GZDoom\\wads"))
 				.stream()
 				.map(wadService::save)
