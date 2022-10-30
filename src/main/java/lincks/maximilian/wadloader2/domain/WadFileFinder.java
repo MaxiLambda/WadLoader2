eb6cb9d998a077d2742146f4310243c5bd36abfc
Maxi <maximilian.lincks@gmx.net>
2022-10-30 13:44:28 +0100

    created WadFileFinder and UnitTests

---
+++ src/main/java/lincks/maximilian/wadloader2/domain/WadFileFinder.java
@@ -0,0 +1,49 @@
+package lincks.maximilian.wadloader2.domain;
+
+import lincks.maximilian.wadloader2.config.WadExtensionConfiguration;
+import lincks.maximilian.wadloader2.model.wads.Wad;
+import lincks.maximilian.wadloader2.repos.services.WadService;
+import lincks.maximilian.wadloader2.utils.PathUtil;
+import lincks.maximilian.wadloader2.utils.StreamUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.List;
+
+/**
+* Service used to search for Wads in the FileSystem
+*/
+@Service
+@RequiredArgsConstructor
+@Log
+public class WadFileFinder {
+
+    private final WadExtensionConfiguration wadExtCfg;
+
+    public List<Wad> findWads(Collection<Path> paths){
+        return paths.stream()
+                .map(this::findWads)
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    public List<Wad> findWads(Path path){
+        List<String> allowedExtensions = wadExtCfg.getAllowed();
+
+        try(var paths = Files.walk(path)){
+            return paths.filter(StreamUtil.filter(
+                    PathUtil::getExtension,
+                    allowedExtensions::contains))
+                    .map(Wad::new)
+                    .toList();
+        }catch (IOException e) {
+            log.severe(e.toString());
+            return List.of();
+        }
+    }
+}
---
+++ src/test/java/lincks/maximilian/wadloader2/domain/WadFileFinderTest.java
@@ -0,0 +1,47 @@
+package lincks.maximilian.wadloader2.domain;
+
+import lincks.maximilian.wadloader2.config.WadExtensionConfiguration;
+import lincks.maximilian.wadloader2.model.wads.Wad;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class WadFileFinderTest {
+
+    @Mock
+    WadExtensionConfiguration wadExtCfg;
+
+    @InjectMocks
+    WadFileFinder wadFileFinder;
+
+    final Path testResourcesPath = Path.of("src/test/resources/testDir");
+    final List<Wad> wads = Stream.of(
+            "src/test/resources/testDir/wad1.pk3",
+            "src/test/resources/testDir/subDir/Wad2.pk3",
+            "src/test/resources/testDir/subDir/Wad3.pk3"
+        )
+            .map(Path::of)
+            .map(Wad::new)
+            .toList();
+
+    @Test
+    void findWads() {
+
+        when(wadExtCfg.getAllowed()).thenReturn(List.of(".pk3"));
+
+        var foundWads = wadFileFinder.findWads(testResourcesPath);
+        assertTrue(foundWads.containsAll(wads));
+        assertEquals(foundWads.size(),wads.size());
+    }
+}
