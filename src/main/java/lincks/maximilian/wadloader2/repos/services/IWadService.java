c440c229d65dde0514382bff6a3c2d0fee4f0a65
Maxi <maximilian.lincks@gmx.net>
2022-11-17 15:17:33 +0100

    changed type of constructor parameter

--- src/main/java/lincks/maximilian/wadloader2/repos/services/IWadService.java
+++ src/main/java/lincks/maximilian/wadloader2/repos/services/IWadService.java
@@ -1,13 +1,13 @@
 package lincks.maximilian.wadloader2.repos.services;
 
 import lincks.maximilian.wadloader2.model.wads.IWad;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lincks.maximilian.wadloader2.repos.IWadRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IWadService extends AbstractService<IWad, String>{
 
-    public IWadService(JpaRepository<IWad, String> repo) {
+    public IWadService(IWadRepo repo) {
         super(repo);
     }
 }
