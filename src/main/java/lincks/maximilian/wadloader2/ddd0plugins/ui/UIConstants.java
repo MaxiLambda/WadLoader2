a943aff1ee383af1f7fcb7a583e7d4cf58c7ee8f
Maxi <maximilian.lincks@gmx.net>
2023-01-18 18:42:39 +0100

    added UIBase and Empty Classes for Pane

---
+++ src/main/java/lincks/maximilian/wadloader2/ddd0plugins/ui/UIConstants.java
@@ -0,0 +1,19 @@
+package lincks.maximilian.wadloader2.ddd0plugins.ui;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UIConstants {
+    public static final String APP_TITLE = "Wad Loader 2";
+    public static final String SETTINGS_TAB = "Settings";
+    public static final String START_GAME_TAB = "Home";
+    public static final String WAD_PACKS_TAB = "Wad-Packs";
+    public static final String RULE_TAB = "Rules";
+
+    public static final String SETTINGS_TAB_TIP = "Manage Settings";
+    public static final String START_GAME_TAB_TIP = "Start Doom";
+    public static final String WAD_PACKS_TAB_TIP = "Manage your Wad-Packs";
+    public static final String RULE_TAB_TIPS = "Write rules for your Wad-Packs";
+
+}
