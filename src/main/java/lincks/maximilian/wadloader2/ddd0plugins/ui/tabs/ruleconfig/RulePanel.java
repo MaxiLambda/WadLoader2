ab816340fbe50a6b88410dea9a87bf78dc41b5f4
Maxi <maximilian.lincks@gmx.net>
2023-01-28 16:31:34 +0100

    refactored to abstract class to force extension on JPanel

--- src/main/java/lincks/maximilian/wadloader2/ddd0plugins/ui/tabs/ruleconfig/RulePanel.java
+++ src/main/java/lincks/maximilian/wadloader2/ddd0plugins/ui/tabs/ruleconfig/RulePanel.java
@@ -2,8 +2,9 @@ package lincks.maximilian.wadloader2.ddd0plugins.ui.tabs.ruleconfig;
 
 import lincks.maximilian.wadloader2.ddd3domain.rules.WadPackRule;
 
+import javax.swing.*;
 import java.util.Optional;
 
-public interface RulePanel {
-    Optional<WadPackRule> getRule();
+public abstract class RulePanel extends JPanel {
+    abstract Optional<WadPackRule> getRule();
 }
